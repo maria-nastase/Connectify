70d72afd01ac714534b6d4910d0b364bb522a587
MclPio <113801014+MclPio@users.noreply.github.com>
2024-10-27 02:04:17 -0400

    Init next

---
+++ app/counter/page.js
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Counter() {
+
+  return (
+    <div>
+      <div>
+        <h1>This is a Counter!!!</h1>
+        <h1></h1>
+
+        <div>
+          <button>Increase</button>
+          <button>Decrease</button>
+          <button>Reset</button>
+        </div>
+      </div>
+    </div>
+  );
+}
